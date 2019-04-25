d838a138f70c8e3e0df583eaa4ed144e80580acb
Alexis Gutierrez <frsknalexis.1997@gmail.com>
2019-04-25 01:42:27 -0500

    Proyecto Base Sistema Facturacion

---
+++ facturacion_backend/src/main/java/com/dev/sfw/FacturacionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionBackendApplication.class, args);
+	}
+
+}
---
+++ facturacion_backend/src/test/java/com/dev/sfw/FacturacionBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.sfw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacturacionBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
